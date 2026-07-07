8d0acf6d8e7139499b4756cc052ac90d7f3eb695
Paberu <paberu.tocador@gmail.com>
2026-07-07 14:19:41 +0300

    Добавил сохранение состояния из класса Tracker, добавление имени привычки, конкретной даты или просто текущей даты.

--- HabitTracker/Storage.py
+++ HabitTracker/Storage.py
@@ -21,16 +21,19 @@ class Storage:
     def check_habit(self, habit) -> bool:
         return habit in self.__data.keys()
 
-    def add_habit_data(self, habit_data: dict):
-        for habit, dates in habit_data.items():
+    def add_habit_name(self, habit_name: str) -> None:
+        if habit_name not in self.__data.keys():
+            self.__data[habit_name] = []
+
+    def add_habit_data(self, habit_name : str, habit_dates: list) -> None:
             try:
-                habit_dates = self.__data[habit]
-                for date in dates:
-                    if date not in habit_dates:
-                        habit_dates.append(date)
-                self.__data[habit] = habit_dates
+                dates = self.__data[habit_name]
+                for date in habit_dates:
+                    if date not in dates:
+                        dates.append(date)
+                self.__data[habit_name] = dates
             except KeyError:
-                self.__data[habit] = dates
+                self.__data[habit_name] = dates
 
     # For test purposes
     def is_empty(self) -> bool:
--- HabitTracker/Tracker.py
+++ HabitTracker/Tracker.py
@@ -1,12 +1,34 @@
+from datetime import date
+
 import rich
 import click
 
+
 from Storage import Storage
+
+class Tracker:
+
+    def __init__(self):
+        self.__storage = Storage()
+
+    def add_habit_name(self, habit_name):
+        self.__storage.add_habit_name(habit_name)
+
+    def set_habit_date(self, habit_name, habit_date):
+        self.__storage.add_habit_data(habit_name, [habit_date])
+
+    def check_habit_today(self, habit_name):
+        self.set_habit_date(habit_name, date.today().strftime("%Y-%m-%d"))
+
+    def save_habits(self):
+        self.__storage.save_habits()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    storage = Storage()
-    storage.add_habit_data({'Спорт': ['2026-06-05', '2026-06-06']})
-    storage.add_habit_data({'Спорт': ['2026-06-05', '2026-06-06']})
-    storage.save_habits()
+    tracker = Tracker()
+    tracker.add_habit_name('Спорт')
+    tracker.set_habit_date('Спорт', '2026-07-06')
+    tracker.set_habit_date('Спорт', '2026-07-07')
+    tracker.check_habit_today('Спорт')
+    tracker.save_habits()
